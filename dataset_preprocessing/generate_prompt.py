925ebc1435ff004e2ffeac89a47c78c0aad21b3f
1170500820 <21s003017@stu.hit.edu.cn>
2022-11-08 01:17:48 +0800

    update: 增加prompt方案

--- dataset_preprocessing/generate_prompt.py
+++ dataset_preprocessing/generate_prompt.py
@@ -4,7 +4,7 @@ prompt型数据大致可以分为：
 1，自然语言形式的prompt
 2，指令式的prompt
 """
-
+import random
 from get_dataset import *
 ins_config = yaml.load(open('instruction_config.yml', 'r', encoding='utf-8'), Loader=yaml.FullLoader)
 from typing import List
@@ -45,6 +45,95 @@ def duie_to_instruction_sample(sample: dict) -> List[dict]:
     }]
 
 
+def duie_to_instruction_find_object(sample: dict) -> List[dict]:
+    """
+    句子中subject的relation是：
+    :param sample:
+    :return:
+    """
+    text, triplets = sample['text'], sample['triplets']
+    if len(triplets) == 0:
+        return []
+    find_object = []
+    subject_and_relation = {}
+    for triplet in triplets:
+        sub_rel = (triplet['subject'], triplet['relation'])
+        if sub_rel not in subject_and_relation:
+            subject_and_relation[sub_rel] = [triplet['object']]
+        else:
+            subject_and_relation[sub_rel].append(triplet['object'])
+    for k, v in subject_and_relation.items():
+        find_object.append({
+            'input': f'{text}|在这句话中，{k[0]}的{k[1]}是：',
+            'target': ','.join(v)
+        })
+    return find_object
+
+
+def duie_to_instruction_find_subject(sample: dict) -> List[dict]:
+    """
+    句子中什么的relation是object：
+    :param sample:
+    :return:
+    """
+    text, triplets = sample['text'], sample['triplets']
+    if len(triplets) == 0:
+        return []
+    find_subject = []
+    object_and_relation = {}
+    for triplet in triplets:
+        ob_rel = (triplet['object'], triplet['relation'])
+        if ob_rel not in object_and_relation:
+            object_and_relation[ob_rel] = [triplet['subject']]
+        else:
+            object_and_relation[ob_rel].append(triplet['subject'])
+    for k, v in object_and_relation.items():
+        find_subject.append({
+            'input': f'{text}|在这句话中，什么的{k[1]}是{k[0]}：',
+            'target': ','.join(v)
+        })
+    return find_subject
+
+
+def duie_to_instruction_find_relation(sample: dict) -> List[dict]:
+    """
+    句子中subject的什么是object：
+    :param sample:
+    :return:
+    """
+    text, triplets = sample['text'], sample['triplets']
+    if len(triplets) == 0:
+        return []
+    find_relation = []
+    subject_and_object = {}
+    for triplet in triplets:
+        sub_ob = (triplet['subject'], triplet['object'])
+        if sub_ob not in subject_and_object:
+            subject_and_object[sub_ob] = [triplet['relation']]
+        else:
+            subject_and_object[sub_ob].append(triplet['relation'])
+    for k, v in subject_and_object.items():
+        find_relation.append({
+            'input': f'{text}|在这句话中，{k[0]}与{k[1]}的关系是：',
+            'target': ','.join(v)
+        })
+    return find_relation
+
+
+def duie_to_instruction_hybrid_find(sample: dict) -> List[dict]:
+    """
+    句子中subject的relation是：
+    :param sample:
+    :return:
+    """
+    choice = random.sample([0, 1, 2], k=1)[0]
+    if choice == 0:
+        return duie_to_instruction_find_object(sample)
+    elif choice == 1:
+        return duie_to_instruction_find_subject(sample)
+    else:
+        return duie_to_instruction_find_relation(sample)
+
 def weiboner_to_instruction():
     datas = get_weiboner()
     train = datas['train'] + datas['dev']  # 将训练集和开发集合并
@@ -58,19 +147,37 @@ def weiboner_to_instruction():
     dump_jsonl(test_modified, os.path.join(path_config['prompted_path'], 'ner_weibo_test.jsonl'))
 
 
-def duie_to_instruction():
+def duie_to_instruction(option='instruction'):
+    """
+
+    :param option: instruction, find_object, find_subject, find_relation, hybrid_find, find_entity
+    :return:
+    """
+    if option == 'instruction':
+        func = duie_to_instruction_sample
+    elif option == 'find_object':
+        func = duie_to_instruction_find_object
+    elif option == 'find_subject':
+        func = duie_to_instruction_find_subject
+    elif option == 'find_relation':
+        func = duie_to_instruction_find_relation
+    elif option == 'hybrid_find':
+        func = duie_to_instruction_hybrid_find
+    else:
+        func = duie_to_instruction_sample
     datas = get_duie()
     train = datas['train']
     dev = datas['dev']
 
     train_modified, dev_modified = [], []
-    for e in train: train_modified.extend(duie_to_instruction_sample(e))
-    for e in dev: dev_modified.extend(duie_to_instruction_sample(e))
+    for e in train: train_modified.extend(func(e))
+    for e in dev: dev_modified.extend(func(e))
 
-    dump_jsonl(train_modified, os.path.join(path_config['prompted_path'], 'duie_train.jsonl'))
-    dump_jsonl(dev_modified, os.path.join(path_config['prompted_path'], 'duie_dev.jsonl'))
+    dump_jsonl(train_modified, os.path.join(path_config['prompted_path'], f'duie_{option}_train.jsonl'))
+    dump_jsonl(dev_modified, os.path.join(path_config['prompted_path'], f'duie_{option}_dev.jsonl'))
 
 if __name__ == '__main__':
     # weiboner_to_instruction()
-    duie_to_instruction()
+    duie_to_instruction('find_relation')
+    duie_to_instruction('hybrid_find')
     print('')
