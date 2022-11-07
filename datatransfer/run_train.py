a95811b0a180bc98c7cd181b482069dcca3eb91d
1170500820 <21s003017@stu.hit.edu.cn>
2022-11-07 14:48:38 +0800

    update: 核心参数可以通过命令行控制了

--- datatransfer/run_train.py
+++ datatransfer/run_train.py
@@ -10,45 +10,62 @@ import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
 
-from datatransfer.naive_t5_2 import T5FineTuner
+from datatransfer.conditional_generation_model import T5FineTuner, BartFineTuner
 
 
-# 一些重要参数需要单独列出
-name = 't5-small-duie'
-batchsize = 3
-n_gpus = 4
-epochs = 4
-conf = dict(
-    # 随机种子
-    seed=42,
+def handle_cli():
+    parser = ArgumentParser()
 
-    # 模型参数
-    model_name='/mnt/huggingface_models/mt5-small',
-    max_seq_length=512,
-    learning_rate=3e-4,
-    weight_decay=0.1,
-    adam_epsilon=1e-3,
-    warmup_steps=0,
+    parser.add_argument('--model', type=str, choices=['bart', 't5'], default='t5')
+    parser.add_argument('--bsz', type=int, default=4)
+    parser.add_argument('--n_gpus', type=int, default=1)
+    parser.add_argument('--epoch', type=int, default=5)
+    parser.add_argument('--name', type=str, default='TaskTransfer_default')
 
-    # 训练参数
-    train_batch_size=batchsize,
-    eval_batch_size=batchsize,
-    max_epochs=epochs,
-    n_gpus=n_gpus,
-    accumulate_grad_batches=4,
-    strategy='ddp',
-    accelerator='gpu',
+    args = vars(parser.parse_args())
 
-    # 日志控制
-    logger_dir='tb_log/',
-    dirpath='t5-checkpoints/',
-    every_n_epochs=1,
+    if args['model'] == 't5':
+        model_name = '/mnt/huggingface_models/mt5-small'
+    else:  # args['model'] == 'bart'
+        model_name = '/mnt/huggingface_models/bart-base-chinese'
 
-    # checkpoint
-    save_top_k=4,
-    monitor='val_loss',
-)
+    # 一些重要参数需要单独列出
 
+    conf = dict(
+        # 随机种子
+        seed=42,
+
+        # 本次训练的任务名字（也就是模型名字））
+        name=args['name'],
+
+        # 模型参数
+        model=args['model'],
+        model_name=model_name,
+        max_seq_length=512,
+        learning_rate=3e-4,
+        weight_decay=0.1,
+        adam_epsilon=1e-3,
+        warmup_steps=0,
+
+        # 训练参数
+        train_batch_size=args['bsz'],
+        eval_batch_size=args['bsz'],
+        max_epochs=args['epoch'],
+        n_gpus=args['n_gpus'],
+        accumulate_grad_batches=4,
+        strategy='ddp',
+        accelerator='gpu',
+
+        # 日志控制
+        logger_dir='tb_log/',
+        dirpath='t5-checkpoints/',
+        every_n_epochs=1,
+
+        # checkpoint
+        save_top_k=4,
+        monitor='val_loss',
+    )
+    return conf
 
 def set_seed(seed):
     random.seed(seed)
@@ -61,7 +78,7 @@ def set_seed(seed):
 def get_logger(config):
     return TensorBoardLogger(
         save_dir=config['logger_dir'],
-        name=name
+        name=config['name']
     )
 
 
@@ -75,16 +92,27 @@ def train(config):
     logger = get_logger(config)
     callbacks = get_callbacks(config)
 
-    train_params = dict(
-        accumulate_grad_batches=config['accumulate_grad_batches'],
-        accelerator=config['accelerator'],
-        devices=config['n_gpus'],
-        max_epochs=config['max_epochs'],
-        strategy=config['strategy'],
-        precision=32,
-        logger=logger,
-        callbacks=callbacks
-    )
+    if config['n_gpus'] == 1:
+        train_params = dict(
+            accumulate_grad_batches=config['accumulate_grad_batches'],
+            accelerator=config['accelerator'],
+            max_epochs=config['max_epochs'],
+            precision=32,
+            logger=logger,
+            callbacks=callbacks
+        )
+    else:  # config['n_gpus'] > 1
+
+        train_params = dict(
+            accumulate_grad_batches=config['accumulate_grad_batches'],
+            accelerator=config['accelerator'],
+            devices=config['n_gpus'],
+            max_epochs=config['max_epochs'],
+            strategy=config['strategy'],
+            precision=32,
+            logger=logger,
+            callbacks=callbacks
+        )
     model_params = dict(
         weight_decay=config['weight_decay'],
         model_name=config['model_name'],
@@ -99,7 +127,11 @@ def train(config):
         eval_batch_size=config['eval_batch_size'] 
     )
 
-    model = T5FineTuner(model_params)
+    if config['model'] == 't5':
+        model = T5FineTuner(model_params)
+    else:  # config['model'] == 'bart'
+        model = BartFineTuner(model_params)
+
     trainer = pl.Trainer(**train_params)
     trainer.fit(model)
 
@@ -108,4 +140,5 @@ def train(config):
 
 
 if __name__ == '__main__':
+    conf = handle_cli()
     train(conf)
